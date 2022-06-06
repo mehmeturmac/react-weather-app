9f5354141fbec014ca868f21c1488d5369f7bf3e
mehmeturmac <mehmeturmac@gmail.com>
2022-06-06 19:35:39 +0300

    localStorage fix

--- src/Context/main.jsx
+++ src/Context/main.jsx
@@ -5,14 +5,16 @@ export const WeatherContext = createContext();
 export const WeatherProvider = ({ children }) => {
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState(
-    JSON.parse(localStorage.getItem("city")) || {
-      id: 1,
-      name: "Adana",
-      latitude: "37.0000",
-      longitude: "35.3213",
-      population: 2183167,
-      region: "Akdeniz",
-    }
+    localStorage.getItem("city")
+      ? JSON.parse(localStorage.getItem("city"))
+      : {
+          id: 1,
+          name: "Adana",
+          latitude: "37.0000",
+          longitude: "35.3213",
+          population: 2183167,
+          region: "Akdeniz",
+        }
   );
 
   const days = [
